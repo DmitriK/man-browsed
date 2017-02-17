5b65c49f5c520d446af27e5956f499b47aa9fb44
Dmitri Kourennyi <dkour@mykolab.com>
2017-02-17 16:51:48 -0500

    Add basic browser instructions to landing page.

--- src/landing.rs
+++ src/landing.rs
@@ -1,15 +1,25 @@
 pub static HTML: &'static str = r#"<!DOCTYPE html>
 <html>
 <head>
-<title>Local Man Page Viewer</title>
-<link rel="search" type="application/opensearchdescription+xml"
-      title="Search Local Man Pages" href="os.xml">
+ <title>Local Man Page Viewer</title>
+ <link rel="search" type="application/opensearchdescription+xml"
+       title="Search Local Man Pages" href="os.xml">
 </head>
 <body>
-<form method="get">
-<input name="q"></input>
-<input type="submit"></input>
-</form>
+ <form method="get">
+  <input name="q"></input>
+  <input type="submit"></input>
+ </form>
+ <h2>Browser Integration</h2>
+ <h3>Firefox</h3>
+ <ul>
+  <li>Click on the search icon in the search bar.</li>
+  <li>Click on 'Add "Search Local Man Pages"' to add the search provider.</li>
+  <li>Click on 'Change Search Settings' button.</li>
+  <li>Double-click the 'Keyword' column for the 'Man Browse' search provider.</li>
+  <li>Type in 'man' or an alternate keyword of your choosing.</li>
+  <li>In the awesomebar, just type in 'man TERM' to instantly get a manual page.</li>
+ </ul>
 </body>
 </html>
 "#;
